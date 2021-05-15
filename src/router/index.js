fa7520c6fd585bd30354f00311ce76fdc322b984
Kaicheng Yang <yangkc@iu.edu>
2021-05-15 11:08:13 -0500

    update personal page stuffs

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
+// Vue related
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import BootstrapVue from 'bootstrap-vue'
+
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../../static/css/style.css'
+
+// Components
+import Home from '@/components/Home'
+import PublistPage from '@/components/PublistPage'
+import NewsPage from '@/components/NewsPage'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: NewsPage
+    },
+    {
+      path: '/pub',
+      name: 'PublistPage',
+      component: PublistPage
     }
   ]
 })
