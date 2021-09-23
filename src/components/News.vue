db7eb736653b12f2d1b37637a27018590eff91ff
Kaicheng Yang <yangkc@iu.edu>
2021-09-23 18:38:36 -0400

    add: add news type

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,7 +2,9 @@
     <div id="newslist">
         <ul class="mt-3" style="padding-left: 0">
             <li v-for="news in news_to_show">
-                <span class="badge" style="display:inline-block; width:100px; background-color: #867d78">{{ news.date }}</span> <span v-html="news.msg"></span>
+                <span class="badge" :style="box_style(color_text_map['date']['color'])">{{ news.date }}</span>
+                <span v-if="news.type" class="badge" :style="box_style(color_text_map[news.type]['color'])">{{ color_text_map[news.type]['text'] }}</span>
+                <span v-html="news.msg"></span>
             </li>
         </ul>
 
@@ -27,7 +29,13 @@ export default {
     },
     data () {
         return {
-            news_list: json
+            news_list: json,
+            color_text_map: {
+                "date": {"color": "#867d78",},
+                "new_paper": {"color": "#ac4142", "text": "New paper",},
+                "new_preprint": {"color": "#006298", "text": "New preprint",},
+                "personal_news": {"color": "#66435A", "text": "Personal",}
+            },
         }
     },
     computed: {
@@ -36,6 +44,15 @@ export default {
                 return this.news_list.slice(0, 5);
             }else{
                 return this.news_list;
+            }
+        }
+    },
+    methods: {
+        box_style (color) {
+            return {
+                "display" : "inline-block",
+                "width": "95px",
+                "background-color": color
             }
         }
     }
