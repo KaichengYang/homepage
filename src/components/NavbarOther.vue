7efea452b74492551b3b9f69a5586fd5f69e9551
Kaicheng Yang <yangkc@iu.edu>
2022-05-21 23:35:45 -0400

    fix: fix the link back to home

--- src/components/NavbarOther.vue
+++ src/components/NavbarOther.vue
@@ -1,7 +1,7 @@
 <template>
     <b-navbar toggleable="md" type="dark" style="background-color: #ac4142;" fixed="top">
     <div class="container">
-        <b-navbar-brand href="/">Home</b-navbar-brand>
+        <router-link to="/"><b-navbar-brand>Home</b-navbar-brand></router-link>
     </div>
     </b-navbar>
 </template>
