54ea8e318022b20eaebb082cb930d32dceffbd93
Kaicheng Yang <yangkc@iu.edu>
2022-07-04 16:44:27 -0400

    add: add refs to media

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -6,7 +6,17 @@
         | {{ media.date }}
         <br>
         <a :href="media.url" target="_blank" class="my-1">{{ media.title }}</a>
-        <!-- <span class="badge" :style="box_style(color_map['date']['color'], 'short')">{{ media.date }}</span> -->
+        <br>
+        <span v-if="!is_home">
+          <p v-if="media.type == 'quotation'">Quoting me</p>
+          <p v-if="media.type == 'coverage' && media.ref">
+            Mentioning
+            <template v-for="(ref, index) in media.ref" :href="ref.link">
+              <template v-if="index > 0"> and </template>
+              <a :href="ref.link" target="_blank"> {{ ref.title }} </a>
+            </template>
+          </p>
+        </span>
       </li>
     </ol>
     <BackForth v-bind:is_home="is_home" v-bind:target="'/media'"/>
@@ -14,7 +24,9 @@
 </template>
 
 <script>
-import json from '../../static/files/media.json'
+import media_json from '../../static/files/media.json'
+import tools_json from '../../static/files/tools.json'
+import refs_json from '../../static/files/refs.json'
 import BackForth from './BackForth.vue'
 
 export default {
@@ -30,7 +42,7 @@ export default {
   },
   data () {
     return {
-      media_list: json,
+      media_list: this.assign_projects_to_media(refs_json, tools_json, media_json),
       color_map: {
         "date": {"color": "#867d78",},
         "outlet": {"color": "#ac4142"}
@@ -47,6 +59,32 @@ export default {
     }
   },
   methods: {
+    assign_projects_to_media: function (refs_json, tools_json, media_json) {
+      media_json.forEach(function (media) {
+        var ref_list = [];
+        tools_json.forEach(function (tool) {
+          if(media.project_id && media.project_id.indexOf(tool.id) >= 0){
+            ref_list.push({
+              "title": tool.title,
+              "link": tool.links[0]
+            });
+          }
+        });
+        refs_json.forEach(function (ref) {
+          if(media.project_id && media.project_id.indexOf(ref.id) >= 0){
+            ref_list.push({
+              "title": ref.title,
+              "link": ref.links[0]
+            });
+          }
+        });
+
+        if(ref_list.length > 0){
+          media["ref"] = ref_list;
+        }
+      });
+      return media_json;
+    },
     box_style (color, length) {
       return {
         "display" : "inline-block",
@@ -61,6 +99,10 @@ export default {
 <style scoped>
 img {
   height: 20px;
+}
+
+span {
+  font-size: 15px;
 }
 
 </style>
