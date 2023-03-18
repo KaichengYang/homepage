d57419b61d4c05d9f9892bd58545428b21d06dc9
Kaicheng Yang <yangkc@iu.edu>
2023-03-18 18:11:50 -0400

    fix: display of links

--- src/components/PublistUnit.vue
+++ src/components/PublistUnit.vue
@@ -16,11 +16,11 @@
           </span>
           <br>
 
-          <span v-if="paper.media">
-            <i>Media Coverage: </i>
-            <span v-for="(media, index) in paper.media" :key="index">
-              <a v-bind:href="media.url" target="_blank">{{ media.outlet }} <i class="fas fa-external-link-alt"></i> </a>
-              <span v-if="index != paper.media.length - 1">| </span>
+          <span v-if="paper.links">
+            🔗
+            <span v-for="(link, index) in paper.links" :key="index">
+              <a v-bind:href="link.url" target="_blank"> {{ link.name }} <i v-bind:class="get_pub_icon(link.name)"></i> </a>
+              <span v-if="index != paper.links.length - 1">| </span>
             </span>
             <br>
           </span>
@@ -30,11 +30,14 @@
               <br>
           </span>
 
-          <span v-if="paper.links">
-            <span v-for="(link, index) in paper.links" :key="index">
-              <a v-bind:href="link.url" target="_blank"> {{ link.name }} <i v-bind:class="get_pub_icon(link.name)"></i> </a>
-              <span v-if="index != paper.links.length - 1">| </span>
+          <span v-if="paper.media">
+            <!-- <i>Media Coverage: </i> -->
+            📰
+            <span v-for="(media, index) in paper.media" :key="index">
+              <a v-bind:href="media.url" target="_blank">{{ media.outlet }} <i class="fas fa-external-link-alt"></i> </a>
+              <span v-if="index != paper.media.length - 1">| </span>
             </span>
+            <br>
           </span>
         </li>
 
