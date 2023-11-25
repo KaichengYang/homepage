12e596625400cc9d977897b9130c159ec0aeddc5
Kaicheng Yang <yangkc@iu.edu>
2023-11-25 11:23:40 -0500

    add: new genai category

--- src/components/Publist.vue
+++ src/components/Publist.vue
@@ -52,11 +52,11 @@ export default {
         conf: "Conference Proceedings",
         journal: "Journal Articles",
         preprint: "Preprints",
-        llm: "LLM",
+        genai: "Generative AI",
         other: "Other Works"
       },
       default_paper_meta_order: {
-        topic: ["socialmedia", "bot", "misinformation", "llm", "politics", "covid", "opioid", "netsci"],
+        topic: ["genai", "socialmedia", "bot", "misinformation", "politics", "covid", "opioid", "netsci"],
         type: ["preprint", "journal", "conf", "other"],
         year: [2023, 2022, 2021, 2020, 2019, 2018, 2017]
       }
