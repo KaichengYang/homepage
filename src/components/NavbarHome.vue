c026bcc6c37ad86870ad9472468b9a944d8cfd5a
Kaicheng Yang <yangkc@iu.edu>
2023-11-30 14:21:54 -0500

    add: link to the blog

--- src/components/NavbarHome.vue
+++ src/components/NavbarHome.vue
@@ -10,6 +10,7 @@
         <b-nav-item href="#" v-scroll-to="'#publication'">Publications</b-nav-item>
         <b-nav-item href="#" v-scroll-to="'#tool'">Tools</b-nav-item>
         <b-nav-item href="#" v-scroll-to="'#media'">Media</b-nav-item>
+        <b-nav-item href="https://yang3kc.substack.com" target="_blank">Blog</b-nav-item>
         <b-nav-item href="static/files/cv.pdf">CV</b-nav-item>
         <b-nav-item href="#" v-scroll-to="'#about'">About</b-nav-item>
       </b-navbar-nav>
